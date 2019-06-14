92fe9148d00b8aefa5e0edca3ca44d906c9b04c0
YoussoufBa <youssoufba97@gmail.com>
2019-06-14 17:03:10 +0200

    Commit initial

---
+++ A/connexion.py
@@ -0,0 +1,13 @@
+export default firebaseConfig = {
+
+  "apiKey": "AIzaSyAZPZLD5Oo8_WRA9ziDLJq_vUVvqT6M0Vk",
+  "authDomain": "cpanel5427.firebaseapp.com",
+  "databaseURL": "https://cpanel5427.firebaseio.com",
+  "projectId": "cpanel5427",
+  "storageBucket": "cpanel5427.appspot.com",
+  "messagingSenderId": "228227778190",
+  "appId": "1:228227778190:web:92c2f36cb913eab7"
+};
+
+firebase=pyrebase.initialize_app(connexion)
+
